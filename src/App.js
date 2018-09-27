069fb78ab9f61cf922fb2bc476b4123e4d9cc251
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-09-27 15:23:15 -0400

    working on fetch

--- src/App.js
+++ src/App.js
@@ -1,100 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import Square from "./Square"
+import axios from "axios"
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      "id": 1,
-      "board": [
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ],
-        [
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " ",
-          " "
-        ]
+      id: 1,
+      board: [
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", "1", "2", "1", "F", " ", " "],
+        [" ", "1", "_", "_", "_", "2", " ", " "],
+        [" ", "1", "_", "_", "_", "1", " ", " "],
+        [" ", " ", "1", "_", "_", "2", " ", " "],
+        [" ", " ", " ", "1", "1", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "]
       ],
+      state: "playing",
+      mines: 9
     }
   }
 
+  _sampleGame = event => {
+    console.log("clicked")
+    axios
+      .get("https://minesweeper-api.herokuapp.com/#game-api-get/games/1")
+      .then(response => {
+        this.setState = response.data
+      })
+  }
+
   render() {
+    // let squareValue = this.state.board.map(element => {
+    //   return <Square />
+    // })
+
     return (
-      <body>
+      <div>
         <header>
           <h1>Minesweeper</h1>
         </header>
@@ -107,101 +52,100 @@ class App extends Component {
         </select>
 
         <main>
-
-          <h2><span>010</span><button>😀</button><span>000</span></h2>
+          <h2>
+            <span>010</span>
+            <button onClick={this._sampleGame} />
+            <span>000</span>
+          </h2>
 
           <table>
             <tbody>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[0][0]} />
+                <Square value={this.state.board[0][1]} />
+                <Square value={this.state.board[0][2]} />
+                <Square value={this.state.board[0][3]} />
+                <Square value={this.state.board[0][4]} />
+                <Square value={this.state.board[0][5]} />
+                <Square value={this.state.board[0][6]} />
+                <Square value={this.state.board[0][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[1][0]} />
+                <Square value={this.state.board[1][1]} />
+                <Square value={this.state.board[1][2]} />
+                <Square value={this.state.board[1][3]} />
+                <Square value={this.state.board[1][4]} />
+                <Square value={this.state.board[1][5]} />
+                <Square value={this.state.board[1][6]} />
+                <Square value={this.state.board[1][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[2][0]} />
+                <Square value={this.state.board[2][1]} />
+                <Square value={this.state.board[2][2]} />
+                <Square value={this.state.board[2][3]} />
+                <Square value={this.state.board[2][4]} />
+                <Square value={this.state.board[2][5]} />
+                <Square value={this.state.board[2][6]} />
+                <Square value={this.state.board[2][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[3][0]} />
+                <Square value={this.state.board[3][1]} />
+                <Square value={this.state.board[3][2]} />
+                <Square value={this.state.board[3][3]} />
+                <Square value={this.state.board[3][4]} />
+                <Square value={this.state.board[3][5]} />
+                <Square value={this.state.board[3][6]} />
+                <Square value={this.state.board[3][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[4][0]} />
+                <Square value={this.state.board[4][1]} />
+                <Square value={this.state.board[4][2]} />
+                <Square value={this.state.board[4][3]} />
+                <Square value={this.state.board[4][4]} />
+                <Square value={this.state.board[4][5]} />
+                <Square value={this.state.board[4][6]} />
+                <Square value={this.state.board[4][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[5][0]} />
+                <Square value={this.state.board[5][1]} />
+                <Square value={this.state.board[5][2]} />
+                <Square value={this.state.board[5][3]} />
+                <Square value={this.state.board[5][4]} />
+                <Square value={this.state.board[5][5]} />
+                <Square value={this.state.board[5][6]} />
+                <Square value={this.state.board[5][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[6][0]} />
+                <Square value={this.state.board[6][1]} />
+                <Square value={this.state.board[6][2]} />
+                <Square value={this.state.board[6][3]} />
+                <Square value={this.state.board[6][4]} />
+                <Square value={this.state.board[6][5]} />
+                <Square value={this.state.board[6][6]} />
+                <Square value={this.state.board[6][7]} />
               </tr>
               <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
+                <Square value={this.state.board[7][0]} />
+                <Square value={this.state.board[7][1]} />
+                <Square value={this.state.board[7][2]} />
+                <Square value={this.state.board[7][3]} />
+                <Square value={this.state.board[7][4]} />
+                <Square value={this.state.board[7][5]} />
+                <Square value={this.state.board[7][6]} />
+                <Square value={this.state.board[7][7]} />
               </tr>
             </tbody>
           </table>
-
         </main>
-      </body >
-    );
+      </div>
+    )
   }
 }
 
-export default App;
-
-
-
+export default App
---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+    render() {
+        return (
+            <td>{this.props.value}</td>
+        );
+    }
+}
+
+export default Square;
